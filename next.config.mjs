8007f1d7ea83a5593226dafa829a79a744954fd7
Sujal Tyagi <147542543+Sujaltyagi007@users.noreply.github.com>
2025-07-27 23:52:06 +0530

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    assetPrefix: 'sujaltyagi007.github.io',
-    basePath: 'sujaltyagi007.github.io',
 };
 
 export default nextConfig;
